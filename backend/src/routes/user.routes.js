d0ac9821f99a730cdfd04aa2b53f808886c1497c
supremozee <supremozee14@gmail.com>
2025-07-02 16:01:22 +0100

    user routes added

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,11 @@
+import express from "express"
+import { followUser, getUserProfile, syncUser, updateProfile } from "../controllers/user.controller.js"
+import { protectedRoute } from "../middleware/auth.middleware.js"
+
+const router = express.Router()
+router.get("/profile/:username", getUserProfile)
+router.post("/sync", protectedRoute, syncUser)
+router.post("/me", protectedRoute, getCurrentUser)
+router.put("/profile", protectedRoute, updateProfile)
+router.post("/follow/:targetUserId", protectedRoute, followUser);
+export default router
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,22 @@
 import express from "express"
 import { ENV } from "./config/env.js"
 import { connectDB } from "./config/db.js"
-
+import cors from "cors"
+import {clerkMiddleware} from "@clerk/express"
+import UserRoute from "./routes/user.routes.js"
 const app = express()
-
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(clerkMiddleware())
+app.get("/", (req, res)=> res.send("Hello"))
+app.use("/api/users", UserRoute)
 const startServer = async()=> {
     await connectDB()
-    app.get("/", (req, res)=> res.send("Hello"))
     app.listen(ENV.PORT, ()=> console.log(`server is up and running on port:${ENV.PORT}`))
 
 }
-startServer().catch(console.error)
+startServer().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
